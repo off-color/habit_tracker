cd83d6342adadcd27e66acdd5da1116c3a7b84aa
off-color <qweslark@yandex.ru>
2020-03-18 22:35:38 +0500

    StringHelper -> plurals

--- app/src/main/java/com/example/habits_tracker/application/HabitsViewHolder.kt
+++ app/src/main/java/com/example/habits_tracker/application/HabitsViewHolder.kt
@@ -3,7 +3,6 @@ package com.example.habits_tracker.application
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.example.habits_tracker.R
-import com.example.habits_tracker.StringHelper
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.habit_layout.*
 
@@ -11,21 +10,22 @@ class HabitsViewHolder(
     override val containerView: View
 ) : RecyclerView.ViewHolder(containerView), LayoutContainer {
 
-    private val stringHelper = StringHelper()
-
     fun bind(habit: Habit, listener: OnItemClickListener, position: Int) {
         containerView.setOnClickListener {
             listener.onItemClick(habit, position)
         }
         titleView.text = habit.title
         periodicityView.text = containerView.context.getString(
-            R.string.countAndPeriodicity,
-            habit.count,
-            stringHelper.getCountWordFor(habit.count),
-            habit.periodicity,
-            stringHelper.getDaysWordFor(habit.periodicity)
+            R.string.countAndPeriodicity, containerView.context.resources.getQuantityString(
+                R.plurals.countPlurals,
+                habit.count,
+                habit.count
+            ), containerView.context.resources.getQuantityString(
+                R.plurals.daysPlurals,
+                habit.periodicity,
+                habit.periodicity
+            )
         )
-        descriptionView.text = habit.description
         typeIcon.setImageResource(
             if (habit.isGood) R.drawable.ic_sentiment_satisfied
             else R.drawable.ic_sentiment_dissatisfied
